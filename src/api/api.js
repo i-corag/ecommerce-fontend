ce0ac245a9d9fb53cfa5bef1daa680e6e0b9add4
i-corag <icoraglio@gmail.com>
2023-05-15 18:51:56 +0200

    axios with credentials

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,11 @@
 import { QueryClient } from '@tanstack/react-query';
 import axios from 'axios';
-import Axios from 'axios';
+//import Axios from 'axios';
 
-Axios.defaults.withCredentials = true; //to be able to read session
+//Axios.defaults.withCredentials = true; //to be able to read session
 
 const URI = axios.create({
+  withCredentials: true,
   baseURL: 'https://ecommerce-backend-production-2b45.up.railway.app/api/',
 });
 
